4f5c475b4bb845e9ef05a6af39f79c6d948a0605
Lajos Meszaros <m_lajos@hotmail.com>
2022-05-27 19:02:49 +0200

    raise precast slots to 9 and add additional key controls for the extra precast slots

--- src/core/Config.cpp
+++ src/core/Config.cpp
@@ -159,6 +159,12 @@ constexpr const ActionKey actions[NUM_ACTION_KEY] = {
 	ActionKey(Keyboard::Key_1), // PRECAST1
 	ActionKey(Keyboard::Key_2), // PRECAST2
 	ActionKey(Keyboard::Key_3), // PRECAST3
+	ActionKey(Keyboard::Key_4), // PRECAST4
+	ActionKey(Keyboard::Key_5), // PRECAST5
+	ActionKey(Keyboard::Key_6), // PRECAST6
+	ActionKey(Keyboard::Key_7), // PRECAST7
+	ActionKey(Keyboard::Key_8), // PRECAST8
+	ActionKey(Keyboard::Key_9), // PRECAST9
 	ActionKey(Keyboard::Key_Tab, Keyboard::Key_NumPad0), // WEAPON
 	ActionKey(Keyboard::Key_F9), // QUICKLOAD
 	ActionKey(Keyboard::Key_F5), // QUICKSAVE
@@ -173,7 +179,7 @@ constexpr const ActionKey actions[NUM_ACTION_KEY] = {
 	ActionKey(Keyboard::Key_Equals), // NEXT
 	ActionKey(Keyboard::Key_C), // CROUCHTOGGLE
 	ActionKey(Keyboard::Key_B), // UNEQUIPWEAPON
-	ActionKey(Keyboard::Key_4), // CANCELCURSPELL
+	ActionKey(), // CANCELCURSPELL (originally Keyboard::Key_4)
 	ActionKey(Keyboard::Key_R, Keyboard::Key_M), // MINIMAP
 	ActionKey((Keyboard::Key_LeftAlt << 16) | Keyboard::Key_Enter, (Keyboard::Key_RightAlt << 16) | Keyboard::Key_Enter), // TOGGLE_FULLSCREEN
 	ActionKey(Keyboard::Key_Grave), // CONSOLE
@@ -300,6 +306,12 @@ constexpr const std::string_view actions[NUM_ACTION_KEY] = {
 	"precast_1",
 	"precast_2",
 	"precast_3",
+	"precast_4",
+	"precast_5",
+	"precast_6",
+	"precast_7",
+	"precast_8",
+	"precast_9",
 	"draw_weapon",
 	"quickload",
 	"quicksave",
