dd86f496c14ccf8eca5b27f9a62affaaef20eb29
Frederik Gelder <frederik.gelder@freenet.de>
2022-10-02 19:02:56 +0200

    add chatmessage

---
+++ src/network/messages/ChatMessage.h
@@ -0,0 +1,26 @@
+#include "network/messages/Message.h"
+
+struct ChatMessage : public Message {
+	std::string sender;
+	std::string message;
+
+	ChatMessage() {}
+
+	ChatMessage(std::string sender, std::string message) : sender(sender), message(message) {
+
+	}
+
+	virtual void send(std::vector<unsigned char>& buffer) {
+		write(sender, buffer);
+		write(message, buffer);
+	}
+
+	virtual void read(const unsigned char* buffer, const size_t bufferLen) {
+		sender = readString(buffer);
+		message = readString(buffer);
+	}
+private:
+	virtual ~ChatMessage() {
+
+	}
+};
